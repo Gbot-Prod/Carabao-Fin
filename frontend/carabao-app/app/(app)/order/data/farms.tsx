b82764a27bcd62f63e0e2c86a3821f60960c6bf1
Gbot-Prod <gilbert.dolz24@gmail.com>
2026-04-24 03:39:46 +0800

    Bugfix for farm import in order page.

---
+++ frontend/carabao-app/app/(app)/order/data/farms.tsx
@@ -0,0 +1,11 @@
+export interface Farm {
+  name: string;
+  rating: number;
+  reviews: string;
+  time: string;
+  badge: string;
+  category: string;
+  deliveryFee: number;
+  promo: string;
+  image: string;
+}
